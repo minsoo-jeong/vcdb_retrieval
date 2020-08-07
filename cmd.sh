a83160230e1be3aa8ea4f93ad1e4f4d49c95fe5f
ms-jeong <jms8167@gmail.com>
2020-08-07 16:52:10 +0900

    cmd

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,8 @@
-python main.py -lr 1e-3 -c mobilenet_sgd_lr_1e-3
+python main.py -lr 1e-4 -c mobilenet_adam_lr_1e-4
+python main.py -lr 1e-5 -c mobilenet_adam_lr_1e-5
+python main.py -lr 1e-6 -c mobilenet_adam_lr_1e-6
+python main.py -lr 1e-5 -c mobilenet_adam_lr_1e-5
+
+python main2.py -lr 1e-4 -c mobilenet_sgd_lr_1e-4
+python main2.py -lr 1e-5 -c mobilenet_sgd_lr_1e-5
+python main2.py -lr 1e-6 -c mobilenet_sgd_lr_1e-6
